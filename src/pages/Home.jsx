d51a8a9cb806129459fe6f80ff816f8d6af4beef
Rhysmond <E1406512@u.nus.edu>
2025-07-12 22:37:19 +0800

    Added cool stars and meteor background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export const Home = () => {
         {/* Theme Toggle */}
         <ThemeToggle />
         {/* Background Effects*/}
-
+        <StarBackground />
         {/* Navbar */}
 
         {/* Main Content */}
