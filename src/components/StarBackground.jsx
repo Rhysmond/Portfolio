3b419582b7127059eb21223567d338e53d57c871
Rhysmond <E1406512@u.nus.edu>
2025-07-12 22:10:47 +0800

    Shifted toggle button to the top right

---
+++ src/components/StarBackground.jsx
@@ -0,0 +1,7 @@
+
+
+export const StarBackground = () => {
+    return <div>
+
+    </div>;
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import {ThemeToggle} from "@/components/ThemeToggle.jsx";
+import {ThemeToggle} from "@/components/ThemeToggle";
+import {StarBackground} from "@/components/StarBackground";
 
 export const Home = () => {
     return <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
