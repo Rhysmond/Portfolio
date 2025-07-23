dc623558950442a4e9018392617e16a5b05af9c3
Rhysmond <E1406512@u.nus.edu>
2025-07-23 22:22:16 +0800

    updated Project image folder name to project to work with vercel

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -46,7 +46,7 @@ export const ProjectsSection = () => {
                     with time, care and attention, I hope you will enjoy them!
                 </p>
 
-                {/*Projects*/}
+                {/*projects*/}
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                     {projects.map((project, key) => (
                         <div
